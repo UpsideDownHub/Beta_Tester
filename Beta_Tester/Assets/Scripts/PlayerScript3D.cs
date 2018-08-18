d05185490c19d3664acfa5770a0ec5825c7d836c
maxnovoguterres <35860401+maxnovoguterres@users.noreply.github.com>
2018-08-18 02:49:02 -0300

    minha fase em 3D

---
+++ Beta_Tester/Assets/Scripts/PlayerScript3D.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript3D : MonoBehaviour {
+
+    SAP2D.SAP2DAgent agent;
+    Rigidbody _Rigidbody;
+    Rigidbody Rigidbody { get { return _Rigidbody ?? GetComponent<Rigidbody>(); } }
+    bool wasTouchingGround = true;
+    bool collided = false;
+
+    void Start()
+    {
+        agent = GetComponent<SAP2D.SAP2DAgent>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Rigidbody.velocity = Vector2.zero;
+            //Rigidbody.angularVelocity = 0;
+            Rigidbody.Sleep();
+        }
+
+        if (collided)
+        {
+            agent.CanMove = true;
+        }
+        else
+            agent.CanMove = false;
+
+        //wasTouchingGround = collider.IsTouchingLayers(ground);
+
+        if (transform.position.y <= -3.56)
+        {
+            print("morreu");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            collided = true;
+        }
+    }
+}
