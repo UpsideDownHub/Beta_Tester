6a59caa9ed6ba9146dc763118aa05c941a0ceb75
maxnovoguterres <35860401+maxnovoguterres@users.noreply.github.com>
2018-08-21 13:37:56 -0300

    checkpoint

---
+++ Beta_Tester/Assets/Scripts/SpinningBall.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinningBall : MonoBehaviour
+{
+    public float spinx;
+    public float spiny;
+    public float spinz;
+
+    private void FixedUpdate()
+    {
+        transform.Rotate(spinx, spiny, spinz);
+    }
+}
