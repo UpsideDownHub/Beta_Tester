2a550f7b828cb0662c40ea82ef7b2faeb62d358e
felipefeio <felipemasuna@gmail.com>
2018-09-28 02:07:48 -0300

    Checkpoint
    Checkpoint

---
+++ Beta_Tester/Assets/Scripts/MoveFireBall.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveFireBall : MonoBehaviour {
+
+    Transform target;
+
+    private void Start()
+    {
+        target = GameObject.Find("Player").GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.position, 10 * Time.deltaTime);
+    }
+}
