1d26d2fe24a845966840a84476c132dc49498d28
maxnovoguterres <35860401+maxnovoguterres@users.noreply.github.com>
2018-11-11 02:25:57 -0200

    checkpoint

--- Beta_Tester/Assets/Scripts/ParticleManager.cs
+++ Beta_Tester/Assets/Scripts/ParticleManager.cs
@@ -3,21 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ParticleManager : MonoBehaviour
-{
-    ParticleSystem p;
-    Animator animator;
+{   
+    //WalkSmokeParticle
+    ParticleSystem particleWalkSmoke;
+    Animator playerAnimator;
+    SpriteRenderer playerSpriterenderer;
+    public Material material;
 
     private void Start()
-    {
-        p = GetComponent<ParticleSystem>();
-        animator = transform.parent.GetComponent<Animator>();
+    {   
+        //WalkSmokeParticle
+        particleWalkSmoke = GetComponent<ParticleSystem>();
+        playerAnimator = transform.parent.GetComponent<Animator>();
+        playerSpriterenderer = transform.parent.GetComponent<SpriteRenderer>();
     }
 
     private void Update()
     {
-        if (animator.GetBool("moving2") && !p.isPlaying)
-            p.Play();
-        else if (!animator.GetBool("moving2") && p.isPlaying)
-            p.Stop();
+        //WalkSmokeParticle
+        if (playerAnimator.GetBool("moving2") && !particleWalkSmoke.isPlaying)
+            particleWalkSmoke.Play();
+        else if (!playerAnimator.GetBool("moving2") && particleWalkSmoke.isPlaying)
+            particleWalkSmoke.Stop();
+
+        if (playerSpriterenderer.flipX)
+        {
+            material.mainTextureScale = new Vector2(-1, 1);
+            particleWalkSmoke.transform.localPosition = new Vector2(0.55f, particleWalkSmoke.transform.localPosition.y);
+            particleWalkSmoke.transform.localRotation = new Quaternion(particleWalkSmoke.transform.localRotation.x, 180, particleWalkSmoke.transform.localRotation.z, particleWalkSmoke.transform.localRotation.w);
+        }
+        else
+        {
+            material.mainTextureScale = new Vector2(1, 1);
+            particleWalkSmoke.transform.localPosition = new Vector2(-0.55f, particleWalkSmoke.transform.localPosition.y);
+            particleWalkSmoke.transform.localRotation = new Quaternion(particleWalkSmoke.transform.localRotation.x, 0, particleWalkSmoke.transform.localRotation.z, particleWalkSmoke.transform.localRotation.w);
+        }
     }
 }
