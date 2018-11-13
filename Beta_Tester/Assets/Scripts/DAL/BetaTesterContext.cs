4f05839c8dcb323b030d88a205efa93169d8e346
maxnovoguterres <35860401+maxnovoguterres@users.noreply.github.com>
2018-11-13 19:58:34 -0200

    criação do DAL

---
+++ Beta_Tester/Assets/Scripts/DAL/BetaTesterContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.DAL
+{
+    public class BetaTesterContext
+    {
+        public static Sql<User> User = new Sql<User>();
+        public static Sql<Phase> Phase = new Sql<Phase>();
+    }
+}
---
+++ Beta_Tester/Assets/Scripts/DAL/User.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.DAL
+{
+    public class User : IdentityUser<int>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
