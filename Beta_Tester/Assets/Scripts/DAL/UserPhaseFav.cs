988953d6542e84c3d49ca203e4516faefe1758c6
bladek8 <lucas1999araujo@gmail.com>
2018-11-20 22:04:33 -0200

    checkpoint

---
+++ Beta_Tester/Assets/Scripts/DAL/UserPhaseFav.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.DAL
+{
+    public class UserPhaseFav
+    {
+        [DisplayName("Key")]
+        public int UserPhaseFavId { get; set; }
+        public int PhaseId { get; set; }
+        public int UserId { get; set; }
+    }
+}
